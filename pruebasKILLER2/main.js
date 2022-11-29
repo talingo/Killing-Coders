926e54a01b69c79bda5632c96ff0940f37babdd1
Fabio Migliaccio <febbio9@gmail.com>
2022-11-29 16:31:22 +0100

    js for creating coders list and array created

--- pruebasKILLER2/main.js
+++ pruebasKILLER2/main.js
@@ -3,26 +3,14 @@ let inputnamebox = document.querySelector("input[type='text']")
 let arrayNombres = [];
 
 function myFunction() {
-    const div = document.createElement("div");
-    div.setAttribute("class", "column");
-    const div2 = document.createElement("div");
-    div2.setAttribute("class", "raw");
+    
     const node = document.createElement("li");
-     const btnlista= document.createElement("button");
-    btnlista.setAttribute("class", "btnli");
-     const salto= document.createElement("br");
-    btnlista.textContent = "-";
+    
     const textnode = document.createTextNode(inputnamebox.value);
     node.appendChild(textnode);
-    document.getElementById("myList").appendChild(div);
-    document.querySelector(".column").appendChild(div2);
+    document.getElementById("myList").appendChild(li);
     
-    document.querySelector(".raw").appendChild(node);
-    document.querySelector(".raw").appendChild(btnlista);
     
-
-   
-
     arrayNombres.push(inputnamebox.value);
     inputnamebox.value="";
     console.log(arrayNombres);
